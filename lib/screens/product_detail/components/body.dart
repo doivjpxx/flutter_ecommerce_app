7829223b9a0f26d9e3360649279579cb3e0fad7b
Huy Phong <doivjpxx@gmail.com>
2021-04-28 21:13:14 +0700

    add product description

--- lib/screens/product_detail/components/body.dart
+++ lib/screens/product_detail/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_shop/models/product.model.dart';
+import 'package:flutter_ecommerce_shop/screens/product_detail/components/product_description.dart';
 import 'package:flutter_ecommerce_shop/screens/product_detail/components/product_image.dart';
+import 'package:flutter_ecommerce_shop/screens/product_detail/components/top_rounded_container.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -8,6 +10,17 @@ class Body extends StatelessWidget {
   const Body({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ProductImage(product: product);
+    return Column(
+      children: [
+        ProductImage(product: product),
+        TopRoundedContainer(
+          color: Colors.white,
+          child: ProductDescription(
+            product: product,
+            onPressed: () {},
+          ),
+        )
+      ],
+    );
   }
 }
