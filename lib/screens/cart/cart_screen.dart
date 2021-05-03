4e2f14e4c5a2eed9fadb203a5e592fd94a3c9789
Huy Phong <doivjpxx@gmail.com>
2021-05-03 22:06:12 +0700

    completed cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_shop/components/default_button.dart';
+import 'package:flutter_ecommerce_shop/constants.dart';
 import 'package:flutter_ecommerce_shop/screens/cart/components/body.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../size_config.dart';
 
@@ -11,6 +14,7 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: Body(),
+      bottomNavigationBar: CheckOutCart(),
     );
   }
 
@@ -27,6 +31,81 @@ class CartScreen extends StatelessWidget {
             style: Theme.of(context).textTheme.caption,
           )
         ],
+      ),
+    );
+  }
+}
+
+class CheckOutCart extends StatelessWidget {
+  const CheckOutCart({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: getProportionateScreenWidth(15),
+          horizontal: getProportionateScreenWidth(30)),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, -15),
+                blurRadius: 20,
+                color: Color(0xFFDADADA).withOpacity(0.15))
+          ]),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: getProportionateScreenWidth(40),
+                  width: getProportionateScreenWidth(40),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF5F6F9),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: SvgPicture.asset("assets/icons/receipt.svg"),
+                ),
+                Spacer(),
+                Text("Add a voucher code"),
+                const SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 12,
+                  color: kTextColor,
+                )
+              ],
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text.rich(TextSpan(text: "Total:\n", children: [
+                  TextSpan(
+                      text: "\$337.15",
+                      style: TextStyle(fontSize: 16, color: Colors.black))
+                ])),
+                SizedBox(
+                  width: getProportionateScreenWidth(190),
+                  child: DefaultButton(
+                    text: "Checkout",
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,70 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_shop/constants.dart';
 import 'package:flutter_ecommerce_shop/models/cart.model.dart';
 import 'package:flutter_ecommerce_shop/size_config.dart';
+import 'package:flutter_svg/svg.dart';
 
-class Body extends StatelessWidget {
+import 'cart_item_card.dart';
+
+class Body extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CartItemCard(cart: demoCarts[0]),
-      ],
-    );
-  }
+  _BodyState createState() => _BodyState();
 }
 
-class CartItemCard extends StatelessWidget {
-  const CartItemCard({Key? key, required this.cart}) : super(key: key);
-
-  final Cart cart;
-
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        SizedBox(
-          width: getProportionateScreenWidth(88),
-          child: AspectRatio(
-            aspectRatio: 0.88,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Color(0xFFF5F6F9),
-                  borderRadius: BorderRadius.circular(15)),
-              child: Image.asset(cart.product.images[0]),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: getProportionateScreenHeight(20),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              cart.product.title,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              ),
-              maxLines: 2,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text.rich(TextSpan(
-                text: "\$${cart.product.price}",
-                style: TextStyle(
-                  color: kPrimaryColor,
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+              key: Key(demoCarts[index].product.id.toString()),
+              direction: DismissDirection.endToStart,
+              background: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xFFFFE6E6)),
+                child: Row(
+                  children: [
+                    Spacer(),
+                    SvgPicture.asset("assets/icons/Trash.svg")
+                  ],
                 ),
-                children: [
-                  TextSpan(
-                      text: " x${cart.numOfItems}",
-                      style: TextStyle(color: kTextColor))
-                ]))
-          ],
-        )
-      ],
+              ),
+              onDismissed: (direction) {
+                setState(() {
+                  demoCarts.removeAt(index);
+                });
+              },
+              child: CartItemCard(cart: demoCarts[index])),
+        ),
+      ),
     );
   }
 }
